b22d86f2d9c1ee114c6df3fe3452b7dff398d9d2
Zaky Naufal A <93167071+zkynaufal@users.noreply.github.com>
2023-12-02 23:02:04 +0700

    Add files via upload

---
+++ src/components/besar/BottomNavigator/index.js
@@ -0,0 +1,64 @@
+import TabItem from "../TabItem";
+import { Box } from "@gluestack-ui/themed";
+
+const BottomNavigator = ({ state, descriptors, navigation }) => {
+  return (
+    <Box
+      position="absolute"
+      left={"$0"}
+      right={"$0"}
+      bottom={"$0"}
+      paddingVertical={"$5"}
+      paddingHorizontal={"$5"}
+      margin={"$2"}
+      flexDirection="row"
+      backgroundColor="$blue"
+      hardShadow="5"
+      borderRadius={"$xl"}
+      justifyContent="space-around"
+    >
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate({ name: route.name, merge: true });
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TabItem
+            key={index}
+            label={label}
+            isFocused={isFocused}
+            onPress={onPress}
+            onLongPress={onLongPress}
+          />
+        );
+      })}
+    </Box>
+  );
+};
+
+export default BottomNavigator;
